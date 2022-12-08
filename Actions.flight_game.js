192fa47791280f908e549d2092c6cab3f934d765
Miku Koskela <Miku.Koskela@metropolia.fi>
2022-12-08 12:38:40 +0200

    update

--- Actions.flight_game.js
+++ Actions.flight_game.js
@@ -1,6 +1,19 @@
+const difficulty = document.querySelector("#dropdown")
+difficulty.addEventListener("click",check_difficulty)
+const play = document.querySelector("#block")
+play.addEventListener("click",login_page)
 
+function check_difficulty() {
+    if (difficulty.value === "Easy") {
+        console.log("Easy")
+    } else if (difficulty.value === "Medium") {
+        console.log("Medium")
+    } else {
+        console.log("Hard")
+    }
+}
+function login_page(){
 
-
-
+}
 
 
